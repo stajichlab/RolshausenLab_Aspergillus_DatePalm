1e5fa8cb25d02e5aa3da4e1275237f37e8931712
Jason Stajich <jasonstajich.phd@gmail.com>
2024-07-07 14:18:27 -0700

    telomere scan update

---
+++ pipeline/stats/02_find_telomeres.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash -l
+#SBATCH -p short -N 1 -n 1 -c 2 --mem 24gb  --out logs/find_telomeres.log
+
+module load parallel
+
+mkdir -p telomere_reports
+for a in $(ls genomes/*.fasta)
+do
+	python scripts_Hiltunen/find_telomeres.py -m 'TA[A]+[C]' $a > telomere_reports/$(basename $a .fasta).telomere_report.txt
+done
